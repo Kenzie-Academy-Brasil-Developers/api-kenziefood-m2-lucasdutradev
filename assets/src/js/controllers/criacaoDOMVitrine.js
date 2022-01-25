d743fc16f4560dab0f54aea0eb3bc3e809eba214
Marcos <marcosmafeimachado@gmail.com>
2022-01-25 11:12:08 -0300

    criado a classe CreateShowcase que monta o layout da vitrine com os produtos

--- assets/src/js/controllers/criacaoDOMVitrine.js
+++ assets/src/js/controllers/criacaoDOMVitrine.js
@@ -1,9 +1,18 @@
 import {RequestProducts} from '../requestFetch.js'
+import {BuildProductLayout} from '../models/modelDOM.js'
+
+const vitrine = document.querySelector('#vitrine')
 
 class CreateShowcase{
     static
-    buildShowcase(){
-        const allProducts = RequestProducts.getProducts()
-        console.log(allProducts)
+    async buildShowcase(){
+        const allProducts = await RequestProducts.getProducts()
+        allProducts.forEach(product=>{
+            const builder = new BuildProductLayout(product)
+            const model = builder.build()
+            vitrine.appendChild(model)
+        })
     }
 }
+
+CreateShowcase.buildShowcase()
