542df357fc11958c653ef04b94c5434615ee2562
Marcos <marcosmafeimachado@gmail.com>
2022-01-25 14:21:14 -0300

    finalizado o filtro por campo de busca. Os produtos são filtrados com base no que o usuário digitou no campo de pesquisa, checando se o nome ou a categoria de alguma produto corresponde com o requisitado.

--- assets/src/js/controllers/criacaoDOMVitrine.js
+++ assets/src/js/controllers/criacaoDOMVitrine.js
@@ -3,39 +3,33 @@ import {BuildProductLayout} from '../models/modelDOM.js'
 
 const vitrine = document.querySelector('#vitrine')
 
+const input = document.querySelector('#searchInput')
+
+input.addEventListener('keypress',function(e){
+    CreateShowcase.filterShowcaseSearch(input.value)
+})
+
 class CreateShowcase{
     static
     async buildShowcase(){
-        vitrine.innerHTML = ''
         const allProducts = await RequestProducts.getProducts()
-        allProducts.forEach(product=>{
-            const builder = new BuildProductLayout(product)
-            const model = builder.buildVitrine()
-            vitrine.appendChild(model)
-        })
+        const builder = new BuildProductLayout(allProducts)
+        vitrine.innerHTML = builder.buildVitrine()
     }
     static
     async filterShowcaseSection(section){
-        vitrine.innerHTML = ''
         const allProducts = await RequestProducts.getProducts()
         const filteredProducts = allProducts.filter(product=>product.categoria == section)
-        filteredProducts.forEach(product=>{
-            const builder = new BuildProductLayout(product)
-            const model = builder.buildVitrine()
-            vitrine.appendChild(model)
-        })
+        const builder = new BuildProductLayout(filteredProducts)
+        vitrine.innerHTML = builder.buildVitrine()
     }
     static
     async filterShowcaseSearch(search){
-        vitrine.innerHTML = ''
         const allProducts = await RequestProducts.getProducts()
-        const filteredProducts = allProducts.filter(product=> product.categoria.toLowerCase().includes(search) || product.nome.toLowerCase().includes(search))
+        const filteredProducts = allProducts.filter(product=> { return product.categoria.toLowerCase().includes(search) || product.nome.toLowerCase().includes(search)})
         if(filteredProducts.length>0){
-            filteredProducts.forEach(product=>{
-                const builder = new BuildProductLayout(product)
-                const model = builder.buildVitrine()
-                vitrine.appendChild(model)
-            })
+            const builder = new BuildProductLayout(filteredProducts)
+            vitrine.innerHTML = builder.buildVitrine()      
         }else{
             vitrine.innerText = `Nenhum Produto foi encontrado`
         }
