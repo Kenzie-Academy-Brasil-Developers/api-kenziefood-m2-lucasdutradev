ef6264c05d2f4acdf6cb8a69fa0b96245734a4b9
Marcos <marcosmafeimachado@gmail.com>
2022-01-25 23:41:40 -0300

    consertando o bug de remover produtos do card

--- assets/src/js/controllers/criacaoDOMCarrinho.js
+++ assets/src/js/controllers/criacaoDOMCarrinho.js
@@ -5,56 +5,53 @@ import { BuildProductLayout } from '../models/modelDOM.js'
 // productTotal.innerText = productsInCard.length
 
 class CreateElementsCards {
+
+    static productsInCard = []
+
     static
         async mountCard(products) {
         const card = document.querySelector("div.emptyCart");
         card.innerHTML = "";
         const newCard = new BuildProductLayout(products);
         card.innerHTML = newCard.buildCard();
+        let buttonRemove = document.querySelectorAll("button.remove--card");
+        console.log(buttonRemove)
+        buttonRemove.forEach((button) => {
+            button.addEventListener("click", () => {
+                this.remove(button,products)
+            });
+        });
     }
 
     static
         async add() {
         let data = await RequestProducts.getProducts()
-        let datasOrder = []
+        let datasOrder
         const buttonAdd = document.querySelectorAll(".addCarrinho");
-        console.log(buttonAdd)
         buttonAdd.forEach((button) => {
             button.addEventListener("click", () => {
-                datasOrder.push(Number(button.closest("div.products").id));
-                let productsInCard = []
+                datasOrder = Number(button.closest("div.products").id);
                 console.log(data)
                 console.log(datasOrder)
-                datasOrder.forEach((order) => productsInCard.push(data.find((product) => product.id === order)))
-                console.log(productsInCard)
-                this.mountCard(productsInCard)
-
-                const buttonRemove = document.querySelectorAll("button.remove--card");
-                console.log(buttonRemove)
-                buttonRemove.forEach((button) => {
-                    button.addEventListener("click", () => {
-                        const boxCard = document.querySelector('.emptyCart');
-                        const childs = boxCard.childNodes;
-                        const divProduct = button.closest("div.products--card");
-                        const index = Array.prototype.indexOf.call(childs, divProduct);
-                        console.log(index);
-                        productsInCard.splice(index, 1);
-                        this.mountCard(productsInCard);
-                    });
-                });
+                this.productsInCard.push(data.find((product) => product.id === datasOrder))
+                console.log(this.productsInCard)
+                this.mountCard(this.productsInCard)
             });
         });
     }
 
-    // static
-    //     async remove() {
-    //     const buttonRemove = document.querySelectorAll("button.remove--card");
-    //     buttonRemove.forEach((button) => {
-    //         button.addEventListener("click", async () => {
-    //             console.log("chamado");
-    //         });
-    //     });
-    // }
+    static
+        remove(button,productsInCard) {
+            console.log('hi')
+            const boxCard = document.querySelector('.emptyCart');
+            const childs = boxCard.childNodes;
+            const divProduct = button.closest("div.products--card");
+            const index = Array.prototype.indexOf.call(childs, divProduct);
+            console.log(index);
+            productsInCard.splice(index, 1);
+            console.log(this.productsInCard)
+            this.mountCard(productsInCard);
+        };
 }
 
 setTimeout(() => {
