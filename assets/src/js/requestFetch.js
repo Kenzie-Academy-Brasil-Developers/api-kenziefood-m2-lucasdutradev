5b4b300aa5100860be177a6980dbf18b0d48ce92
LucasDutra <lucasbryan0217@gmail.com>
2022-01-26 18:44:43 -0300

    criando o POST

--- assets/src/js/requestFetch.js
+++ assets/src/js/requestFetch.js
@@ -1,17 +1,43 @@
+import { captureInputs } from "./models/modelInputsFetch.js"
 
 class RequestProducts {
     static
         async getProducts() {
-            const response = await fetch(`https://kenzie-food-api.herokuapp.com/product`)
-            const processedProducts = await response.json();
-            return processedProducts
+        const response = await fetch(`https://kenzie-food-api.herokuapp.com/product`)
+        const processedProducts = await response.json();
+        return processedProducts
     }
     static
         async getProduct(id) {
-            const response = await fetch(`https://kenzie-food-api.herokuapp.com/product/${id}`)
-            const processedProduct = await response.json();
-            return processedProduct
+        const response = await fetch(`https://kenzie-food-api.herokuapp.com/product/${id}`)
+        const processedProduct = await response.json();
+        return processedProduct
+    }
+
+    static
+        async postMyProducts(data) {
+        console.log(data)
+        console.log(JSON.stringify({"nome": 'Coca cola', "preco": 12, "categoria": 'bebidas', "imagem": 'https://google.com.br', "descricao": 'bla bla bla'}))
+        const response = await fetch(`https://kenzie-food-api.herokuapp.com/my/product`, {
+            method: 'POST',
+            headers: {"Content-Type": "application/json", Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTY0MzExODAwMiwiZXhwIjoxNjQzOTgyMDAyLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.eJRSB14eZyAje7x5c6W7u9UXJdr-_mFCVoGALXzwyJI" },
+            body: JSON.stringify(data)
+        });
+        const processedProducts = await response.json();
+        return processedProducts
+    }
+
+    static
+        async getMyProducts() {
+        const response = await fetch(`https://kenzie-food-api.herokuapp.com/my/product`, {
+            method: 'GET',
+            headers: { Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImlhdCI6MTY0MzExODAwMiwiZXhwIjoxNjQzOTgyMDAyLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.eJRSB14eZyAje7x5c6W7u9UXJdr-_mFCVoGALXzwyJI" },
+        });
+        const processedProducts = await response.json();
+        console.log(processedProducts)
+        return processedProducts
     }
 }
 
-export{RequestProducts}
+
+export { RequestProducts }
