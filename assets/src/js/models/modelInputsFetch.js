9d4ff310ffef15e43bcdbe23f6e8d4f18b01383e
LucasDutra <lucasbryan0217@gmail.com>
2022-01-27 00:04:59 -0300

    criando as regras de negocio do admin

--- assets/src/js/models/modelInputsFetch.js
+++ assets/src/js/models/modelInputsFetch.js
@@ -11,6 +11,7 @@ class captureInputs {
         const buttonAdd = document.querySelector("#sendAdd");
         const buttonDelet = document.querySelector("#sendDelet");
         const buttonUpdate = document.querySelector("#sendUpdate");
+        this.captureInputsForm()
         selector.addEventListener("click", () => {
             if (selector.value === "add") {
                 adicionar.classList.remove("hidden");
@@ -19,7 +20,6 @@ class captureInputs {
                 buttonUpdate.classList.add("hidden");
                 delet.classList.add("hidden");
                 update.classList.add("hidden");
-                this.captureInputsForm()
             }
             if (selector.value === "delete") {
                 delet.classList.remove("hidden")
@@ -45,6 +45,19 @@ class captureInputs {
         const buttonAdd = document.querySelector("#sendAdd");
         const buttonDelet = document.querySelector("#sendDelet");
         const buttonUpdate = document.querySelector("#sendUpdate");
+        const buttonClose = document.querySelectorAll(".close");
+        const modal = document.querySelector(".addi");
+        const modalDelet = document.querySelector(".Deletar");
+        const modalPatch = document.querySelector(".Atualizar");
+        const modalSucess = document.querySelector(".sucess");
+        buttonClose.forEach((button) => {
+            button.addEventListener("click", () => {
+                modal.classList.add("hidden");
+                modalDelet.classList.add("hidden");
+                modalPatch.classList.add("hidden");
+                modalSucess.classList.add("hidden");
+            });
+        });
         buttonAdd.addEventListener("click", (event) => {
             event.preventDefault()
             const name = document.querySelector("#name").value
@@ -52,6 +65,9 @@ class captureInputs {
             const descricao = document.querySelector("#descricao").value
             const preco = document.querySelector("#preco").value
             const image = document.querySelector("#image").value
+            if (name === "" || categoria === "" || descricao === "" || preco === "" || image === "") {
+                return modal.classList.remove("hidden");
+            }
             const data = {
                 "nome": name,
                 "preco": Number(preco),
@@ -60,13 +76,20 @@ class captureInputs {
                 "descricao": descricao
             }
             RequestProducts.postMyProducts(data);
+            return modalSucess.classList.remove("hidden");
         });
-        buttonDelet.addEventListener("click", (event) => {
+        buttonDelet.addEventListener("click", async (event) => {
             event.preventDefault()
             const id = document.querySelector("#id").value;
+            let data = await RequestProducts.getMyProducts();
+            let obj = data.find((valor) => valor.id === Number(id))
+            if (obj === undefined) {
+                return modalDelet.classList.remove("hidden");
+            }
             RequestProducts.deletMyProducts(Number(id));
+            return modalSucess.classList.remove("hidden");
         });
-        buttonUpdate.addEventListener("click", (event) => {
+        buttonUpdate.addEventListener("click", async (event) => {
             event.preventDefault()
             const idPath = document.querySelector("#idPath").value
             const name = document.querySelector("#namePath").value
@@ -74,6 +97,14 @@ class captureInputs {
             const descricao = document.querySelector("#descricaoPath").value
             const preco = document.querySelector("#precoPath").value
             const image = document.querySelector("#imagePath").value
+            let datas = await RequestProducts.getMyProducts();
+            let obj = datas.find((valor) => valor.id === Number(idPath))
+            if (name === "" && categoria === "" && descricao === "" && preco === "" && image === "") {
+                return modalPatch.classList.remove("hidden");
+            }
+            if (obj === undefined) {
+                return modalPatch.classList.remove("hidden");
+            }
             const data = {
                 "nome": name,
                 "preco": Number(preco),
@@ -81,7 +112,20 @@ class captureInputs {
                 "imagem": image,
                 "descricao": descricao
             }
-            RequestProducts.patchMyProducts(data, Number(idPath))
+            if(preco === ""){
+                data["preco"] = obj.preco;
+            }
+            if(categoria === ""){
+                data["categoria"] = obj.categoria;
+            }
+            if(descricao === ""){
+                data["descricao"] = obj.descricao;
+            }
+            if(image === ""){
+                data["imagem"] = obj.imagem;
+            }
+            RequestProducts.patchMyProducts(data, Number(idPath));
+            return modalSucess.classList.remove("hidden");
         });
     }
 }
